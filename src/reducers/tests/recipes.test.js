c22da0619d1ad671d1158d3387d9a1ae461cbf9b
Anthony Damico <anthony.damico721@gmail.com>
2019-05-03 07:33:53 -0400

    added more tests

--- src/reducers/tests/recipes.test.js
+++ src/reducers/tests/recipes.test.js
@@ -1,4 +1,9 @@
 import recipesReducer from '../recipes';
+import {
+    fetchRecipesRequest,
+    fetchRecipesSuccess,
+    fetchRecipesError
+} from '../../actions/recipes';
 
 describe('Recipes Reducer', function() {
     const initialState = {
@@ -24,6 +29,19 @@ describe('Recipes Reducer', function() {
         const currentState = {};
         const state = recipesReducer(currentState, { type: '__UNKNOWN' });
         expect(state).toEqual(currentState);
+    });
+
+    describe('fetchRecipesRequest', function() {
+        it('Should make request', function() {
+            const state = recipesReducer(initialState, fetchRecipesRequest());
+            expect(state).toEqual({
+                recipes: [],
+                currentRecipe: null,
+                loading: true,
+                feedback: null,
+                error: null 
+            });
+        });
     });
 
 });
