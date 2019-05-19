e2a93d73b4b21ec86ba191dfde56f0713a59738f
Anthony Damico <anthony.damico721@gmail.com>
2019-05-18 21:14:21 -0400

    setting up initial page structure

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class App extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Hello!</h1>
-            </div>
+            <React.Fragment>
+                <header className="main-header">
+                    <div>
+                        <h1 className="main-header__brand">Anthony & Britt's Recipes</h1>
+                    </div>
+                    <nav className="main-nav">
+                        <ul className="main-nav__items">
+                            <li className="main-nav__item">
+                                <Link to="/">Our Recipes</Link>
+                            </li>
+                            <li className="main-nav__item">
+                                <Link to="/">Add Recipe</Link>
+                            </li>
+                            <li className="main-nav__item">
+                                <Link to="/">Edit Recipe</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
+                <main>
+                    <section id="recipes">
+                        <article className="recipe">
+                            <h2>Recipe Title</h2>
+                            <h3>Recipe Meal</h3>
+                            <h3>Recipe Type</h3>
+                        </article>
+                        <article className="recipe">
+                            <h2>Recipe Title</h2>
+                            <h3>Recipe Meal</h3>
+                            <h3>Recipe Type</h3>
+                        </article>
+                        <article className="recipe">
+                            <h2>Recipe Title</h2>
+                            <h3>Recipe Meal</h3>
+                            <h3>Recipe Type</h3>
+                        </article>
+                        <article className="recipe">
+                            <h2>Recipe Title</h2>
+                            <h3>Recipe Meal</h3>
+                            <h3>Recipe Type</h3>
+                        </article>
+                    </section>
+                </main>
+                <footer className="main-footer">
+                    <p className="main-footer__info">Copyright &copy; 2019 Anthony D'Amico</p>
+                </footer>
+            </React.Fragment>
         );
     }
 }
