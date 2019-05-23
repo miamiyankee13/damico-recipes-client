9fc837c7414d5a90bdaed4a3cb06b92dabe39625
Anthony Damico <anthony.damico721@gmail.com>
2019-05-23 14:34:04 -0400

    added background image

--- src/components/recipes-page.js
+++ src/components/recipes-page.js
@@ -4,14 +4,19 @@ import RecipesItem from './recipes-item';
 export default class RecipesPage extends React.Component {
     render() {
         return (
-            <section className="recipes">
-                <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
-                <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
-                <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
-                <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
-                <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
-                <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
-            </section>
+            <React.Fragment>
+                <section className="banner">
+                    
+                </section>
+                <section className="recipes">
+                    <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
+                    <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
+                    <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
+                    <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
+                    <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
+                    <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
+                </section>
+            </React.Fragment>
         );
     }
 }
