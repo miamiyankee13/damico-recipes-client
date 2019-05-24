5b6779d53727ef09933bacf10c3678e2bd3fc27f
Anthony Damico <anthony.damico721@gmail.com>
2019-05-24 19:43:24 -0400

    minor changes

--- src/components/recipes-item.js
+++ src/components/recipes-item.js
@@ -3,17 +3,16 @@ import React from 'react';
 export default class RecipesItem extends React.Component {
     render() {
         return (
-            <article className="recipes__item">
-                <h2 className="recipes__item--title">{this.props.recipeTitle}</h2>
-                <h3 className="recipes__item--meal">{this.props.recipeMeal}</h3>
-                <h3 className="recipes__item--type">{this.props.recipeType}</h3>
-                <button 
-                    className="button" 
-                    data-index={this.props.index} 
-                    onClick={this.props.onClick}
-                >
-                View Recipe
-                </button>
+            <article 
+                className="recipes__item" 
+                data-index={this.props.index} 
+                onClick={this.props.onClick}
+            >
+                <h2 className="recipes__item--title" >{this.props.recipeTitle}</h2>
+                <h3 className="recipes__item--heading">Meal</h3>
+                <p className="recipes__item--meal">{this.props.recipeMeal}</p>
+                <h3 className="recipes__item--heading">Type</h3>
+                <p className="recipes__item--type">{this.props.recipeType}</p>
             </article>
         );
     }
