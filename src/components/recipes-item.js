e0fac6ed257287a93b2119f5f98ded95cbe7f39e
Anthony Damico <anthony.damico721@gmail.com>
2019-05-24 15:53:30 -0400

    connected recipes page

--- src/components/recipes-item.js
+++ src/components/recipes-item.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class RecipesItem extends React.Component {
     render() {
         return (
-            <article className="recipes__item">
+            <article className="recipes__item" data-index={this.props.index}>
                 <h2 className="recipes__item--title">{this.props.recipeTitle}</h2>
                 <h3 className="recipes__item--meal">{this.props.recipeMeal}</h3>
                 <h3 className="recipes__item--type">{this.props.recipeType}</h3>
--- src/components/recipes-page.js
+++ src/components/recipes-page.js
@@ -1,20 +1,46 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import RecipesItem from './recipes-item';
+import { fetchRecipes } from '../actions/recipes';
 
-export default class RecipesPage extends React.Component {
+export class RecipesPage extends React.Component {
+    componentDidMount() {
+        this.props.dispatch(fetchRecipes());
+    }
+    
     render() {
+        if (this.props.lodaing || this.props.recipes.length < 0) {
+            return <p>Loading...</p>
+        }
+
+        if (this.props.error) {
+            return <p>{this.props.error}</p>
+        }
+
+        const recipes = this.props.recipes.map((recipe, index) => {
+            return <RecipesItem 
+                        key={`recipe-${index}`} 
+                        index={index} recipeTitle={recipe.name} 
+                        recipeMeal={recipe.meal} 
+                        recipeType={recipe.type}
+                    />
+        });
+        
         return (
             <React.Fragment>
                 <section className="banner"></section>
                 <section className="recipes">
-                    <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
-                    <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
-                    <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
-                    <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
-                    <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
-                    <RecipesItem recipeTitle="Recipe Title" recipeMeal="Recipe Meal" recipeType="Recipe Type"/>
+                    {recipes}
                 </section>
             </React.Fragment>
         );
     }
 }
+
+const mapStateToProps = state => ({
+    loading: state.loading,
+    error: state.error,
+    recipes: state.recipes
+});
+
+export default connect(mapStateToProps)(RecipesPage);
