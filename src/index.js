e50c47ea16f588a0a9a22e946da175eaee49edb3
Anthony Damico <anthony.damico721@gmail.com>
2019-05-24 15:23:07 -0400

    added redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router} from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/app';
+import store from './store';
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>, 
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>, 
 document.getElementById('root'));
