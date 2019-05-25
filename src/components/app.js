56508a33ea3925a4ded4508096fa9b64db6ceba0
Anthony Damico <anthony.damico721@gmail.com>
2019-05-24 23:25:35 -0400

    removed connect from main app

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import Backdrop from './backdrop';
 import Header from './header';
@@ -51,4 +50,4 @@ export class App extends React.Component {
     }
 }
 
-export default withRouter(connect()(App));
+export default withRouter(App);
