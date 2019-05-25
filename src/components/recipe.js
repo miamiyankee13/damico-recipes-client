ebaad1d7f2d702b4bc10422f8626087dbc53f723
Anthony Damico <anthony.damico721@gmail.com>
2019-05-25 14:46:36 -0400

    re-structured recipe page

--- src/components/recipe.js
+++ src/components/recipe.js
@@ -5,22 +5,22 @@ export default class Recipe extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <article className="recipe__info">
-                    <h2 className="recipe__info--title">{this.props.recipeTitle}</h2>
-                    <h3 className="recipe__info--heading">sides</h3>
-                    <ul className="recipe__info--sides">
-                        {this.props.sides}
-                    </ul>
-                </article>
-                <article className="recipe__details">
-                    <h3 className="recipe__details--heading">ingredients</h3>
-                    <ul className="recipe__details--ingredients">
+                <article className="recipe__left">
+                    <h2 className="recipe__title">{this.props.recipeTitle}</h2>
+                    <h3 className="recipe__heading">ingredients</h3>
+                    <ul className="recipe__ingredients">
                         {this.props.ingredients}
                     </ul>
-                    <h3 className="recipe__details--heading">instructions</h3>
-                    <ol className="recipe__details--instructions">
+                </article>
+                <article className="recipe__right">
+                    <h3 className="recipe__heading">instructions</h3>
+                    <ol className="recipe__instructions">
                         {this.props.instructions}
                     </ol>
+                    <h3 className="recipe__heading">sides</h3>
+                    <ul className="recipe__sides">
+                        {this.props.sides}
+                    </ul>
                 </article>
             </React.Fragment>
         );
