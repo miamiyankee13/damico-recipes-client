ef17af079bae22640e91ae78119f17ceb6ec3b41
Anthony Damico <anthony.damico721@gmail.com>
2019-05-27 13:15:04 -0400

    added scrolltop to header

--- src/components/header.js
+++ src/components/header.js
@@ -4,11 +4,21 @@ import Nav from './nav';
 import './styles/header.css';
 
 export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.scrollTop = this.scrollTop.bind(this);
+    }
+
+    scrollTop() {
+        window.scrollTo(0,0);
+    }
+    
     render() {
         return (
             <header className="header">
                 <div className="header__brand">
-                    <Link to="/">
+                    <Link to="/" onClick={this.scrollTop}>
                         <h1 className="header__brand--heading">D'Amico Recipes</h1>
                     </Link>
                 </div>
