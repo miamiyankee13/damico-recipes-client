1ea531a847d8950271f15dcdbe3216c091347436
Anthony Damico <anthony.damico721@gmail.com>
2019-05-30 08:37:04 -0400

    created production build and updated .gitignore.

---
+++ build/precache-manifest.994a16e40022c2b1ba2a5603a012e9b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecf42014e9d72054514a",
+    "url": "/static/css/main.305d2366.chunk.css"
+  },
+  {
+    "revision": "ecf42014e9d72054514a",
+    "url": "/static/js/main.b79ffef5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "51ffdd97b6d99ceb54d6",
+    "url": "/static/js/2.bc151cc1.chunk.js"
+  },
+  {
+    "revision": "e4fc94622833fd05934e3f9d3c3584b6",
+    "url": "/index.html"
+  }
+];
