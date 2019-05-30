68c425bda01b87184b9200b3938de7901929ddcf
Anthony Damico <anthony.damico721@gmail.com>
2019-05-30 10:50:53 -0400

    added back servicew worker

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/app';
 import store from './store';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <Provider store={store}>
@@ -13,3 +14,8 @@ ReactDOM.render(
         </Router>
     </Provider>, 
 document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
