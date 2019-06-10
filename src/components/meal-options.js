8648429073c3f144a71d7e39af92a5329335af62
Anthony Damico <anthony.damico721@gmail.com>
2019-06-10 07:49:52 -0400

    added more comments

--- src/components/meal-options.js
+++ src/components/meal-options.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const MealOptions = () => {
+    
+    //create list of meal options
     const mealOptionsArray = ['Breakfast', 'Dessert', 'Dinner', 'Lunch', 'Snack'];
-
     const mealOptions = mealOptionsArray.map((meal, index) => {
         const mealVal = meal.toLocaleLowerCase()
         return <option key={`meal-${index}`} value={mealVal}>{meal}</option>
