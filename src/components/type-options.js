d15675d67ce7e4007dc29b814f611755550598a8
Anthony Damico <anthony.damico721@gmail.com>
2019-06-12 07:11:30 -0400

    added more comments

--- src/components/type-options.js
+++ src/components/type-options.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const TypeOptions = () => {
+    
+    //create list of type options
     const typeOptionsArray = ['Beef', 'Chicken', 'Eggs', 'Fruit', 'Pancakes', 'Pasta', 'Pizza', 'Pork', 'Sandwiches', 'Vegetables'];
-        
     const typeOptions = typeOptionsArray.map((type, index) => {
         const typeVal = type.toLocaleLowerCase()
         return <option key={`type-${index}`} value={typeVal}>{type}</option>
