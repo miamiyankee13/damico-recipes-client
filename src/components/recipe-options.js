62a9911f081a484e7b7ca8a8e8ac96f48e599e71
Anthony Damico <anthony.damico721@gmail.com>
2019-06-13 07:26:51 -0400

    added more comments

--- src/components/recipe-options.js
+++ src/components/recipe-options.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const RecipeOptions = props => {
+    
+    //create list of recipe options
     const recipeOptions = props.recipes.map((recipe, index) => {
         return <option key={`recipe-${index}`} value={index}>{recipe.name}</option>;
     });
